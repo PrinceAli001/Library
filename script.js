fa812bbc226bf97af093c39c14305d4a6fc8e2d2
Mahfouz <mafz060706@gmail.com>
2024-03-03 18:04:45 +0100

    Book creation with user's input

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let inputTitle = document.querySelector('#title')
 let inputPages = document.querySelector('#pages')
 let inputStatus = document.querySelector('#read-status')
 let btn = document.querySelector('#btn')
-
+let bookShelf = document.querySelector('#book-shelf')
 
 
 const myLibrary = [];
@@ -34,7 +34,36 @@ function addBookToLibrary() {
 
       myLibrary.push(book)
       console.log(myLibrary)
-      console.log(book.status())
+      myLibrary.forEach((element) => {
+        let card = document.createElement('div')
+        card.setAttribute('style','border: 1px solid black; width: 150px; height: fit-content; border-radius: 5px; box-shadow: 2px 2px 5px black; padding: 10px;')
+
+        let pictureHolder = document.createElement('div')
+        pictureHolder.setAttribute('style','border: 1px solid black; height: 100px;')
+
+        let pTitle = document.createElement('p')
+        pTitle.setAttribute('style','margin: 5px;')
+        pTitle.textContent = `Title: ${book.title}`
+
+        let pAuthor = document.createElement('p')
+        pAuthor.setAttribute('style','margin: 5px;')
+        pAuthor.textContent = `Author: ${book.author}`
+
+        let pPages = document.createElement('p')
+        pPages.setAttribute('style','margin: 5px;')
+        pPages.textContent = `Pages: ${book.noOfPages}`
+
+        let pStatus = document.createElement('p')
+        pStatus.setAttribute('style','margin: 5px;')
+        pStatus.textContent = `Status: ${book.status()}`
+
+        card.appendChild(pictureHolder)
+        card.appendChild(pTitle)
+        card.appendChild(pAuthor)
+        card.appendChild(pPages)
+        card.appendChild(pStatus)
+        bookShelf.appendChild(card)
+      })
     })
 }
 addBookToLibrary()
